e657ab12d6a4d2edff59035c8dde62ac4d48165b
yangweili <963492977@qq.com>
2021-03-11 17:19:19 +0800

    update

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-01-31 17:37:32
  * @LastEditors: ywl
- * @LastEditTime: 2021-03-11 09:40:23
+ * @LastEditTime: 2021-03-11 17:19:12
  * @Description: 用户管理
  */
 import http from '@/plugins/axios'
@@ -14,7 +14,7 @@ export default {
     return http.get('/user/getMenuById', { params: data })
   },
   getUserList: (data) => {
-    return http.post('/user/getUserList', data)
+    return http.post('/user/list', data)
   },
   addUserInfo: (data) => {
     return http.post('/user/add', data)
